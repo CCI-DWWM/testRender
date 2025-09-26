fef0e2fc9179bfb6b7929041ecd817d3a8a3a319
goudot <Emmanuel.Goudot@gmail.com>
2025-09-26 08:22:48 +0200

    Landing page ajoutée

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from typing import Union
 
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import RedirectResponse
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World, Emmanuel."}
+    return RedirectResponse("index.html")
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+app.mount("/", StaticFiles(directory="static"), name="static")
